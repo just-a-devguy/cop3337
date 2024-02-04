2e512b169a7c7241db966c90dda05f73e78f6021
Adler Luders <adlerluders@gmail.com>
2024-02-03 19:17:25 -0500

    completed assignment 4 covering polymorphism

--- Assignment 4/src/EMS.java
+++ Assignment 4/src/EMS.java
@@ -6,8 +6,6 @@
  * Employee Management System
  */
 
-import java.util.*;
-
 // Parent Employee
 class Employee {
   int employeeId;
@@ -103,19 +101,52 @@ class Clerk extends Employee {
 }
 
 public class EMS {
+
+  public static void display(Employee emp) {
+    System.out.println("Employee ID: " + emp.getEmployeeId());
+    System.out.println("Name: " + emp.getName());
+    System.out.println("Department: " + emp.getDepartment());
+    System.out.println("Salary: " + emp.getSalary());
+    System.out.println("Designation: " + emp.getDesignation());
+  }
+
+  public static double calculateDeduction(int daysCount) {
+    return 150.0 * daysCount;
+  }
+
   public static void main(String[] args) {
     Employee employee = new Employee(1, "John", "HR", 3000.00, "Employee");
     Manager manager = new Manager(2, "Alice", "Finance", 4000.00, "Manager");
     Clerk clerk = new Clerk(3, "Bob", "Admin", 2500.0, "Clerk");
 
+    System.out.println("Sample output: ");
+    display(employee);
+    System.out.println("");
+    display(manager);
+    System.out.println("");
+    display(clerk);
+
+    System.out.println("------------------------------------------------");
+
     System.out.println("Salaries with bonuses: ");
     employee.addBonus();
     manager.addBonus();
     clerk.addBonus();
-    System.out.println(employee.getName() + " Salary with Bonus: " + employee.salary);
-    System.out.println(manager.getName() + " Salary with Bonus: " + manager.salary);
-    System.out.println(clerk.getName() + " Salary with Bonus: " + clerk.salary);
+    System.out.println(employee.getName() + " Salary with Bonus: " + employee.getSalary());
+    System.out.println(manager.getName() + " Salary with Bonus: " + manager.getSalary());
+    System.out.println(clerk.getName() + " Salary with Bonus: " + clerk.getSalary());
 
-    System.out.println(employee.equals(manager, manager));
+    double empDeduction = calculateDeduction(2);
+    double manDeduction = calculateDeduction(3);
+    double clkDeduction = calculateDeduction(4);
+
+    System.out.println("------------------------------------------------");
+    System.out.println("Deductions: ");
+    System.out.println(employee.getName() + " Deductions was absent for 2 days: $" + empDeduction);
+    System.out.println(manager.getName() + " Deductions was absent for 3 days: $" + manDeduction);
+    System.out.println(clerk.getName() + " Deductions was absent for 4 days: $" + clkDeduction);
+    double total = empDeduction + manDeduction + clkDeduction;
+
+    System.out.println("Total Deductions: " + total);
   }
 }
