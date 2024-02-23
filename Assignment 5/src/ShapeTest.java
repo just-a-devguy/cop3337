4bc22ea0d299b5787c8263e778cc2836f5cb092a
Adler Luders <adlerluders@gmail.com>
2024-02-22 23:19:23 -0500

    assignment 5 covering interface and abstract

---
+++ Assignment 5/src/ShapeTest.java
@@ -0,0 +1,37 @@
+/**
+ * Adler G Luders
+ * COP 3337
+ * Assignment 5
+ * 22 February 2024
+ */
+
+abstract class Shape {
+  String name;
+  String color;
+
+  public abstract void calculateArea();
+
+  public abstract void calculatePerimeter();
+
+}
+
+class Circle extends Shape {
+  double radius;
+
+  // Constructor
+  public Circle(double radius) {
+  }
+
+  // methods
+  public void calculateArea() {
+
+  }
+
+  public void calculatePerimeter() {
+
+  }
+}
+
+public class Main {
+
+}
